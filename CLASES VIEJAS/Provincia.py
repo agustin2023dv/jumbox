d29ed3885c3ca389a702696af08ad86aa59eaf49
agustin2023dv <115184058+agustin2023dv@users.noreply.github.com>
2023-11-01 09:56:30 +0100

    change: refactorizacion clase Provincia

--- CLASES VIEJAS/Provincia.py
+++ CLASES VIEJAS/Provincia.py
@@ -7,50 +7,80 @@ class Provincia:
         - nombre_provincia (str): Nombre de la provincia.
 
     Métodos:
-        - __init__(id_provincia, nombre_provincia): Inicializa un objeto Provincia.
-        - __str__(): Devuelve una representación en cadena de los atributos de la provincia.
-        - get_id_provincia(): Obtiene el ID de la provincia.
-        - get_nombre_provincia(): Obtiene el nombre de la provincia.
-        - set_id_provincia(id_provincia): Establece el ID de la provincia.
-        - set_nombre_provincia(nombre_provincia): Establece el nombre de la provincia.
+        - __init__(self, id_provincia, nombre_provincia): Inicializa un objeto Provincia.
+        - __str__(self): Devuelve una representación en cadena de los atributos de la provincia.
+        - guardar(self, db): Guarda la provincia en la base de datos.
+        - actualizar(self, db): Actualiza la provincia en la base de datos.
+        - eliminar(self, db): Elimina la provincia de la base de datos.
+        - obtener_por_id(cls, db, id_provincia): Obtiene una provincia por su ID desde la base de datos.
+
+    Uso:
+        Esta clase se utiliza para representar y gestionar provincias en una base de datos.
     """
-    
+
     def __init__(self, id_provincia, nombre_provincia):
         """
         Inicializa un objeto Provincia con los atributos proporcionados.
         """
-        self.__id_provincia = id_provincia
-        self.__nombre_provincia = nombre_provincia
+        self.id_provincia = id_provincia
+        self.nombre_provincia = nombre_provincia
 
     def __str__(self):
         """
-        Devuelve una representación de tipo string de la provincia.
+        Devuelve una representación en cadena de la provincia.
         """
-        return f"ID Provincia: {self.__id_provincia}\n" \
-               f"Nombre Provincia: {self.__nombre_provincia}"
+        return f"ID Provincia: {self.id_provincia}\n" \
+               f"Nombre Provincia: {self.nombre_provincia}"
 
-    # Getters
-    def get_id_provincia(self):
+    def guardar(self, db):
         """
-        Obtiene el ID de la provincia.
+        Guarda la provincia en la base de datos.
         """
-        return self.__id_provincia
+        try:
+            consulta = "INSERT INTO provincia (nombre_provincia) VALUES (?)"
+            parametros = (self.nombre_provincia,)
+            db.ejecutar_consulta(consulta, parametros)
+            db.conexion.commit()
+        except Exception as e:
+            print(f"Error al guardar la provincia: {str(e)}")
 
-    def get_nombre_provincia(self):
+    def actualizar(self, db):
         """
-        Obtiene el nombre de la provincia.
+        Actualiza la provincia en la base de datos.
         """
-        return self.__nombre_provincia
+        try:
+            consulta = "UPDATE provincia SET nombre_provincia=? WHERE id_provincia=?"
+            parametros = (self.nombre_provincia, self.id_provincia)
+            db.ejecutar_consulta(consulta, parametros)
+            db.conexion.commit()
+        except Exception as e:
+            print(f"Error al actualizar la provincia: {str(e)}")
 
-    # Setters
-    def set_id_provincia(self, id_provincia):
+    def eliminar(self, db):
         """
-        Establece el ID de la provincia.
+        Elimina la provincia de la base de datos.
         """
-        self.__id_provincia = id_provincia
+        try:
+            consulta = "DELETE FROM provincia WHERE id_provincia = ?"
+            parametros = (self.id_provincia,)
+            db.ejecutar_consulta(consulta, parametros)
+            db.conexion.commit()
+        except Exception as e:
+            print(f"Error al eliminar la provincia: {str(e)}")
 
-    def set_nombre_provincia(self, nombre_provincia):
+    @classmethod
+    def obtener_por_id(cls, db, id_provincia):
         """
-        Establece el nombre de la provincia.
+        Obtiene una provincia por su ID desde la base de datos.
         """
-        self.__nombre_provincia = nombre_provincia
+        try:
+            consulta = "SELECT * FROM provincia WHERE id_provincia = ?"
+            parametros = (id_provincia,)
+            resultado = db.ejecutar_consulta(consulta, parametros)
+            if resultado:
+                fila = resultado.fetchone()
+                if fila:
+                    return cls(*fila)
+        except Exception as e:
+            print(f"Error al obtener la provincia por ID: {str(e)}")
+        return None
