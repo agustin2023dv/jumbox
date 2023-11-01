79ad2e9b58b852a797703ab875e8a44551ce0f1a
agustin2023dv <115184058+agustin2023dv@users.noreply.github.com>
2023-11-01 09:51:09 +0100

    change: refactorizacion de clase CategoriaProducto. add:agregacion hoja de estilos cs

--- CLASES VIEJAS/CategoriaProducto.py
+++ CLASES VIEJAS/CategoriaProducto.py
@@ -1,6 +1,4 @@
 class CategoriaProducto:
-
-
     """
     Clase que representa una categoría de productos en un supermercado.
 
@@ -9,77 +7,71 @@ class CategoriaProducto:
         - nombre (str): Nombre de la categoría.
         - descripcion (str): Descripción de la categoría.
 
-    
     Métodos:
-        - __init__(id_categoria, nombre, descripcion): Inicializa un objeto CategoriaProducto.
-        - __str__(): Devuelve una representación en cadena de los atributos de la categoría.
-        - get_id_categoria(): Obtiene el ID de la categoría.
-        - get_nombre(): Obtiene el nombre de la categoría.
-        - get_descripcion(): Obtiene la descripción de la categoría.
-        - set_id_categoria(id_categoria): Establece el ID de la categoría.
-        - set_nombre(nombre): Establece el nombre de la categoría.
-        - set_descripcion(descripcion): Establece la descripción de la categoría.
+        - __init__(self, id_categoria, nombre, descripcion): Inicializa un objeto CategoriaProducto.
+        - __str__(self): Devuelve una representación en cadena de los atributos de la categoría.
+        - guardar(self, db): Guarda la categoría en la base de datos.
+        - actualizar(self, db): Actualiza la categoría en la base de datos.
+        - eliminar(self, db): Elimina la categoría de la base de datos.
+        - obtener_por_id(cls, db, id_categoria): Obtiene una categoría por su ID desde la base de datos.
 
     Uso:
-        Esta clase se utiliza para representar y organizar diferentes categorías de productos del supermercado.
+        Esta clase se utiliza para representar y gestionar categorías de productos en un supermercado.
     """
-
 
     def __init__(self, id_categoria, nombre, descripcion):
         """
         Inicializa un objeto CategoriaProducto con los atributos proporcionados.
         """
-
         self.id_categoria = id_categoria
         self.nombre = nombre
         self.descripcion = descripcion
 
     def __str__(self):
-
         """
-        Devuelve una representación de tipo string de la categoría.
+        Devuelve una representación en cadena de la categoría.
         """
-
         return f"ID Categoría: {self.id_categoria}\n" \
                f"Nombre: {self.nombre}\n" \
                f"Descripción: {self.descripcion}"
-    
 
+    def guardar(self, db):
+        """
+        Guarda la categoría en la base de datos.
+        """
+        consulta = "INSERT INTO categoria_producto (nombre, descripcion) VALUES (?, ?)"
+        parametros = (self.nombre, self.descripcion)
+        db.ejecutar_consulta(consulta, parametros)
+        db.conexion.commit()
 
-    # Getters
-    def get_id_categoria(self):
+    def actualizar(self, db):
         """
-        Obtiene el ID de la categoría.
+        Actualiza la categoría en la base de datos.
         """
-        return self.id_categoria
+        consulta = "UPDATE categoria_producto SET nombre=?, descripcion=? WHERE id_categoria_producto=?"
+        parametros = (self.nombre, self.descripcion, self.id_categoria)
+        db.ejecutar_consulta(consulta, parametros)
+        db.conexion.commit()
 
-    def get_nombre(self):
+    def eliminar(self, db):
         """
-        Obtiene el nombre de la categoría.
+        Elimina la categoría de la base de datos.
         """
-        return self.nombre
+        consulta = "DELETE FROM categoria_producto WHERE id_categoria_producto = ?"
+        parametros = (self.id_categoria,)
+        db.ejecutar_consulta(consulta, parametros)
+        db.conexion.commit()
 
-    def get_descripcion(self):
+    @classmethod
+    def obtener_por_id(cls, db, id_categoria):
         """
-        Obtiene la descripción de la categoría.
+        Obtiene una categoría por su ID desde la base de datos.
         """
-        return self.descripcion
-
-    # Setters
-    def set_id_categoria(self, id_categoria):
-        """
-        Establece el ID de la categoría.
-        """
-        self.id_categoria = id_categoria
-
-    def set_nombre(self, nombre):
-        """
-        Establece el nombre de la categoría.
-        """
-        self.nombre = nombre
-
-    def set_descripcion(self, descripcion):
-        """
-        Establece la descripción de la categoría.
-        """
-        self.descripcion = descripcion
+        consulta = "SELECT * FROM categoria_producto WHERE id_categoria_producto = ?"
+        parametros = (id_categoria,)
+        resultado = db.ejecutar_consulta(consulta, parametros)
+        if resultado:
+            fila = resultado.fetchone()
+            if fila:
+                return cls(*fila)
+        return None
--- app.py
+++ app.py
@@ -6,9 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/admin_dashboard')
+def admin_dashboard():
+    return render_template('admin_dashboard.html')
+
 if __name__ == '__main__':
-    app.run(debug=True)
-
-
-
-app.run(host='0.0.0.0',port=81,debug=True)
+    app.run(host='0.0.0.0', port=81, debug=True)
