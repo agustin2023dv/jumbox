e65641646177cc00feeb79e87d5110cd487105c8
agustin2023dv <115184058+agustin2023dv@users.noreply.github.com>
2023-11-02 10:07:05 +0100

    fix: correcciones en BD scheme e inserts. Faltan retoques

--- bd/inserts_db.py
+++ bd/inserts_db.py
@@ -40,6 +40,23 @@ categoria_producto_data = [
 for categoria in categoria_producto_data:
     cursor.execute("INSERT INTO categoria_producto (nombre, descripcion) VALUES (?, ?)", categoria)
 
+# Inserts para la tabla producto
+producto_data = [
+    ('Manzanas', 'Manzanas frescas', 2.5, 1),  # Producto 1, perteneciente a la categoría 1
+    ('Peras', 'Peras jugosas', 2.0, 1),  # Producto 2, perteneciente a la categoría 1
+    ('Bananas', 'Bananas maduras', 1.8, 1),  # Producto 3, perteneciente a la categoría 1
+    ('Zanahorias', 'Zanahorias frescas', 1.2, 2),  # Producto 4, perteneciente a la categoría 2
+    ('Tomates', 'Tomates orgánicos', 2.0, 2),  # Producto 5, perteneciente a la categoría 2
+    ('Filete de Res', 'Filete de res de alta calidad', 10.5, 3),  # Producto 6, perteneciente a la categoría 3
+    ('Pechuga de Pollo', 'Pechuga de pollo sin hueso', 8.0, 3),  # Producto 7, perteneciente a la categoría 3
+    ('Leche', 'Leche entera', 1.5, 4),  # Producto 8, perteneciente a la categoría 4
+    ('Queso Cheddar', 'Queso Cheddar en bloque', 3.0, 4),  # Producto 9, perteneciente a la categoría 4
+    ('Pan Blanco', 'Pan blanco recién horneado', 2.0, 5)  # Producto 10, perteneciente a la categoría 5
+]
+
+for producto in producto_data:
+    cursor.execute("INSERT INTO producto (nombre, descripcion, precio, categoria_id) VALUES (?, ?, ?, ?)", producto)
+
 
 # Ejecutar las inserciones para la tabla sexo
 sexo_data = [
@@ -67,7 +84,6 @@ provincia_data = [
     ('Río Negro'),
     ('Neuquén'),
     ('Formosa')
-
 ]
 
 for provincia in provincia_data:
@@ -92,7 +108,6 @@ ciudad_data = [
     ('Formosa', 15),
     ('La Plata', 1),  # Ejemplo de ciudad en la provincia de Buenos Aires
     ('Mar del Plata', 1),  # Ejemplo de ciudad en la provincia de Buenos Aires
-
 ]
 
 for ciudad in ciudad_data:
@@ -127,80 +142,78 @@ for domicilio in domicilio_data:
 
 # Ejecutar las inserciones para la tabla sucursal con FK en domicilio
 sucursal_data = [
-    ( 1, 'jumbox1@jumbox.com', '+1234567890'),
-    ( 2, 'jumbox2@jumbox.com', '+9876543210'),
-    ( 3, 'jumbox3@jumbox.com', '+1112223333'),
-    ( 4, 'jumbox4@jumbox.com', '+4445556666'),
-    ( 5, 'jumbox5@jumbox.com', '+7778889999'),
-    ( 6, 'jumbox6@jumbox.com', '+2223334444'),
-    ( 7, 'jumbox7@jumbox.com', '+5556667777'),
-    ( 8, 'jumbox8@jumbox.com', '+9990001111'),
+    ('jumbox1@jumbox.com', '+1234567890', 1),
+    ('jumbox2@jumbox.com', '+9876543210', 2),
+    ('jumbox3@jumbox.com', '+1112223333', 3),
+    ('jumbox4@jumbox.com', '+4445556666', 4),
+    ('jumbox5@jumbox.com', '+7778889999', 5),
+    ('jumbox6@jumbox.com', '+2223334444', 6),
+    ('jumbox7@jumbox.com', '+5556667777', 7),
+    ('jumbox8@jumbox.com', '+9990001111', 8),
 ]
 
 for sucursal in sucursal_data:
-    cursor.execute("INSERT INTO sucursal (domicilio_id, email, telefono) VALUES (?, ?, ?)", sucursal)
+    cursor.execute("INSERT INTO sucursal (email, telefono, domicilio_id) VALUES (?, ?, ?)", sucursal)
 
 # Ejemplo de inserciones para la tabla empleado_sucursal con sucursal_id y domicilio_id únicos
 empleado_sucursal_data = [
-    ('Juan', 'González', 1, '1985-03-15', '2023-01-10', 1, 1),  # Empleado 1 en Sucursal 1 y Domicilio 1
-    ('María', 'Martínez', 2, '1987-05-20', '2023-01-15', 2, 2),  # Empleado 2 en Sucursal 2 y Domicilio 2
-    ('Carlos', 'López', 1, '1984-07-25', '2023-01-20', 3, 3),  # Empleado 3 en Sucursal 3 y Domicilio 3
-    ('Ana', 'Fernández', 2, '1989-09-30', '2023-01-25', 4, 4),  # Empleado 4 en Sucursal 4 y Domicilio 4
-    ('Diego', 'Rodríguez', 1, '1983-11-05', '2023-01-30', 5, 5),  # Empleado 5 en Sucursal 5 y Domicilio 5
-    ('Laura', 'Gómez', 2, '1986-01-10', '2023-02-05', 6, 6),  # Empleado 6 en Sucursal 6 y Domicilio 6
-    ('Pablo', 'Díaz', 1, '1982-03-15', '2023-02-10', 7, 7),  # Empleado 7 en Sucursal 7 y Domicilio 7
-    ('Silvia', 'Pérez', 2, '1988-05-20', '2023-02-15', 8, 8),  # Empleado 8 en Sucursal 8 y Domicilio 8
-    ('Jorge', 'Fernández', 1, '1981-07-25', '2023-02-20', 9, 9),  # Empleado 9 en Sucursal 9 y Domicilio 9
-    ('Cecilia', 'Martínez', 2, '1990-09-30', '2023-02-25', 10, 10),  # Empleado 10 en Sucursal 10 y Domicilio 10
-    ('Luis', 'González', 1, '1980-11-05', '2023-03-01', 11, 11),  # Empleado 11 en Sucursal 11 y Domicilio 11
-    ('Valeria', 'López', 2, '1991-01-10', '2023-03-05', 12, 12),  # Empleado 12 en Sucursal 12 y Domicilio 12
-    ('Martín', 'Sánchez', 1, '1979-03-15', '2023-03-10', 13, 13),  # Empleado 13 en Sucursal 13 y Domicilio 13
-    ('Natalia', 'García', 2, '1992-05-20', '2023-03-15', 14, 14),  # Empleado 14 en Sucursal 14 y Domicilio 14
-    ('Gustavo', 'Fernández', 1, '1978-07-25', '2023-03-20', 15, 15),  # Empleado 15 en Sucursal 15 y Domicilio 15
-    ('Rosa', 'Díaz', 2, '1993-09-30', '2023-03-25', 16, 16),  # Empleado 16 en Sucursal 16 y Domicilio 16
+    ('Juan', 'González', 1, '1985-03-15', '2023-01-10', 1, 1),
+    ('María', 'Martínez', 2, '1987-05-20', '2023-01-15', 2, 2),
+    ('Carlos', 'López', 1, '1984-07-25', '2023-01-20', 3, 3),
+    ('Ana', 'Fernández', 2, '1989-09-30', '2023-01-25', 4, 4),
+    ('Diego', 'Rodríguez', 1, '1983-11-05', '2023-01-30', 5, 5),
+    ('Laura', 'Gómez', 2, '1986-01-10', '2023-02-05', 6, 6),
+    ('Pablo', 'Díaz', 1, '1982-03-15', '2023-02-10', 7, 7),
+    ('Silvia', 'Pérez', 2, '1988-05-20', '2023-02-15', 8, 8),
+    ('Jorge', 'Fernández', 1, '1981-07-25', '2023-02-20', 9, 9),
+    ('Cecilia', 'Martínez', 2, '1990-09-30', '2023-02-25', 10, 10),
+    ('Luis', 'González', 1, '1980-11-05', '2023-03-01', 11, 11),
+    ('Valeria', 'López', 2, '1991-01-10', '2023-03-05', 12, 12),
+    ('Martín', 'Sánchez', 1, '1979-03-15', '2023-03-10', 13, 13),
+    ('Natalia', 'García', 2, '1992-05-20', '2023-03-15', 14, 14),
+    ('Gustavo', 'Fernández', 1, '1978-07-25', '2023-03-20', 15, 15),
+    ('Rosa', 'Díaz', 2, '1993-09-30', '2023-03-25', 16, 16),
 ]
 
 for empleado in empleado_sucursal_data:
     cursor.execute("INSERT INTO empleado_sucursal (nombre, apellido, sexo_id, fecha_nac, fecha_incorporacion, domicilio_id, sucursal_id) VALUES (?, ?, ?, ?, ?, ?, ?)", empleado)
 
-
-# Inserts para la tabla stock_producto
 # Inserts para la tabla stock_producto con producto_id (donde aplicable)
 stock_producto_data = [
     (80, 8, 4, 1),    # Producto 1 en unidad de medida 4
     (120, 12, 5, 2),  # Producto 2 en unidad de medida 5
     (70, 7, 1, 3),    # Producto 3 en unidad de medida 1
-    (90, 9, 2, 6),  # 
-    (60, 6, 3, 4),  # 
-    (150, 15, 4, 7), # 
-    (110, 11, 5, 8), #
-    (100, 10, 1, 9), # 
-    (130, 13, 2, 10), # 
-    (85, 8, 3, 11),   # 
-    (95, 9, 4, 12),   # 
-    (75, 7, 5, 13),   # 
+    (90, 9, 2, 6),    # Producto 4 en unidad de medida 2
+    (60, 6, 3, 4),    # Producto 5 en unidad de medida 3
+    (150, 15, 4, 7),  # Producto 6 en unidad de medida 4
+    (110, 11, 5, 8),  # Producto 7 en unidad de medida 5
+    (100, 10, 1, 9),  # Producto 8 en unidad de medida 1
+    (130, 13, 2, 10), # Producto 9 en unidad de medida 2
+    (85, 8, 3, 11),   # Producto 10 en unidad de medida 3
+    (95, 9, 4, 12),   # Producto 11 en unidad de medida 4
+    (75, 7, 5, 13),   # Producto 12 en unidad de medida 5
 ]
+
 for stock in stock_producto_data:
     cursor.execute("INSERT INTO stock_producto (stock, stock_minimo, unidad_medida_id, producto_id) VALUES (?, ?, ?, ?)", stock)
 
 
 # Inserts adicionales para la tabla sucursal_stock
 sucursal_stock_data = [
-    (6, 6),   # Sucursal 1 con stock_producto 6
-    (7, 7),   # Sucursal 1 con stock_producto 7
-    (8, 8),   # Sucursal 1 con stock_producto 8
-    (9, 9),   # Sucursal 2 con stock_producto 9
-    (10, 10), # Sucursal 2 con stock_producto 10
-    (11, 11), # Sucursal 2 con stock_producto 11
-    (12, 12), # Sucursal 3 con stock_producto 12
-    (13, 13), # Sucursal 3 con stock_producto 13
-    (14, 14), # Sucursal 3 con stock_producto 14
-    (15, 15), # Sucursal 4 con stock_producto 15
+    (1, 6),   # Sucursal 1 con stock_producto 6
+    (1, 7),   # Sucursal 1 con stock_producto 7
+    (1, 8),   # Sucursal 1 con stock_producto 8
+    (2, 9),   # Sucursal 2 con stock_producto 9
+    (2, 10),  # Sucursal 2 con stock_producto 10
+    (2, 11),  # Sucursal 2 con stock_producto 11
+    (3, 12),  # Sucursal 3 con stock_producto 12
+    (3, 13),  # Sucursal 3 con stock_producto 13
+    (3, 14),  # Sucursal 3 con stock_producto 14
+    (4, 15),  # Sucursal 4 con stock_producto 15
 ]
 
 for sucursal_stock in sucursal_stock_data:
     cursor.execute("INSERT INTO sucursal_stock (sucursal_id, stock_id) VALUES (?, ?)", sucursal_stock)
-
 
 # Guardar los cambios y cerrar la conexión a la base de datos
 conn.commit()
